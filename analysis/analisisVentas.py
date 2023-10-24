83952ef95d1b4dfabacf51ad3628094962e69c91
manunovoa <manunovoa0415@gmail.com>
2023-10-24 07:37:53 -0500

    feat: create boars

--- analysis/analisisVentas.py
+++ analysis/analisisVentas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-
 from helpers.crearCSVVentas import crearCSVVentas
+from helpers.crearTablaHTML import crearTabla
 
 from data.ventas import ventas
 
@@ -10,7 +10,9 @@ crearCSVVentas(ventas,'ventas.csv')
 
 "#2 Cargo la fuente de datos y con pandos PANDAS creo un DATAFRAME"
 ventasDataFrame=pd.read_csv('data/ventas.csv')
-print(ventasDataFrame)
+crearTabla(ventasDataFrame, 'tablaventas')
+
+#print(ventasDataFrame)
 
 #3.Explorar los datos
 examen1 = ventasDataFrame.head()
@@ -24,17 +26,4 @@ examen6 = ventasDataFrame.tail(50)
 
 #5. aplicar modelos estadísticos
 
-#6. Presentar y explorar losd datos
-print(examen1)
-print("\n")
-print(examen2)
-print("\n")
-print(examen3)
-print("\n")
-print(examen4)
-print("\n")
-print(examen5)
-print("\n")
-print(examen6)
-print("\n")
-
+#6. Presentar y explorar los datos
